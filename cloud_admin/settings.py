3704aa81839f824334eaa92bc1bde3742e23b37c
azez0 <nanquery@gmail.com>
2023-03-04 15:51:27 +0100

    fix typo

--- cloud_admin/settings.py
+++ cloud_admin/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from private_data import PRIVATE
+import private_data
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = PRIVATE['SECRET_KEY']
+SECRET_KEY = private_data.PRIVATE['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
