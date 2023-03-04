9c30760a6de11c9d975ade68b4faae08d772d8a3
azez0 <nanquery@gmail.com>
2023-03-04 15:25:14 +0100

    add commented code for debug purpose

--- manage.py
+++ manage.py
@@ -4,9 +4,20 @@ import os
 import sys
 
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_admin.settings')
+
+    #Check DJANGO_SETTINGS_MODULE configuration
+    #if os.environ.get('DJANGO_SETTINGS_MODULE'):
+        # If `subcommand` is missing due to misconfigured settings, the
+        # following line will retrigger an ImproperlyConfigured exception
+        # (get_commands() swallows the original one) so the user is
+        # informed about it.
+        #sys.stderr.write("SETTINGS: "+os.environ.get('DJANGO_SETTINGS_MODULE')+'\n')
+    #else:
+        #sys.stderr.write("No Django settings specified.\n")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
