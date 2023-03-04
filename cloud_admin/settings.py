8210e92b0c3de3ae8d7eafb59284ad7bc1839f31
azez0 <nanquery@gmail.com>
2023-03-04 15:49:38 +0100

    fix typo

--- cloud_admin/settings.py
+++ cloud_admin/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from cloud_admin.private_data import PRIVATE
+from private_data import PRIVATE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
