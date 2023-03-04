cd62f76331e65a76623881c28afca43ddb8f520e
azez0 <nanquery@gmail.com>
2023-03-04 13:12:12 +0100

    fix typo

--- cloud_admin/settings.py
+++ cloud_admin/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from cloud_admin.cloud_admin.private_data import PRIVATE
+from cloud_admin.private_data import PRIVATE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user_service'
+    'user_service',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
